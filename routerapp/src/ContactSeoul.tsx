0009bad4417493deb132290713bfe04580db1763
rudals5215 <rudals5215@naver.com>
2025-10-20 15:29:36 +0900

    feat: carfront project creation

---
+++ routerapp/src/ContactSeoul.tsx
@@ -0,0 +1,7 @@
+function ContactSeoul() {
+  return(
+    <h1>서울</h1>
+  );
+}
+
+export default ContactSeoul;
---
+++ thirdpartyapp/src/App.tsx
@@ -0,0 +1,13 @@
+import DatePicker from 'react-date-picker'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <DatePicker></DatePicker>
+    </>
+  )
+}
+
+export default App
