961611a68f9c3601ffbfd09780bf6ce51f6a17f5
rudals5215 <rudals5215@naver.com>
2025-10-21 17:49:20 +0900

    feat: 20251021 back-front connection using mui / usequery / usemutation

--- carfront/src/components/Carlist.tsx
+++ carfront/src/components/Carlist.tsx
@@ -1,18 +1,57 @@
-import { useQuery } from "@tanstack/react-query";
-import { CarResponse } from "../types";
-import axios from "axios";
+// import { CarResponse } from "../types";  table 태그에서는 data.map() 때문에 필요하지만, x-data-grid 사용 이후로는 필요 없기 때문에 주석처리
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/carapi";
+import { DataGrid, GridColDef, GridCellParams } from "@mui/x-data-grid";
+import { Snackbar } from "@mui/material";
+import { useState } from "react";
+import AddCar from "./AddCar";
 
 function Carlist () {
-  const getCars = async () : Promise<CarResponse[]> => {
-    const response = await axios.get("http://localhost:8080/api/cars");
-
-    return response.data._embedded.cars;
-  }
-
+  const [ open, setOpen ] = useState(false);
+  const queryClient = useQueryClient();
   const { data, error, isSuccess } = useQuery({
     queryKey: ["cars"],
     queryFn: getCars
   });
+
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess: () => {
+      // 자동차 삭제 이후 실행되는 로직
+      setOpen(true);
+      queryClient.invalidateQueries({ queryKey: ["cars"]}); // 이 부분은 useQuery()를 정의한 부분과 관련있습니다.
+    },
+    onError: err=> {
+      console.log(err);
+    },
+  })
+
+  const columns : GridColDef[] = [
+    {field: 'brand', headerName: 'Brand', width: 200},
+    {field: 'model', headerName: 'Model', width: 200},
+    {field: 'color', headerName: 'Color', width: 200},
+    {field: 'registrationNumber', headerName: 'Reg.nr', width: 150},
+    {field: 'modelYear', headerName: 'Model Year', width: 150},
+    {field: 'price', headerName: 'Price', width: 150},
+    {
+      field: 'delete',
+      headerName: '',
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        // 함수의 이름만 전달하기 위해 argument가 없는 화살표함수를 씀.
+        <button 
+          onClick={() => {
+            if (window.confirm(`${params.row.brand}의 ${params.row.model} 자동차를 삭제하시겠습니까?`)){
+            mutate(params.row._links.self.href)}}
+          }
+        >
+          Delete
+        </button>
+      )
+    }
+  ];
 
   if (!isSuccess) {
     return <span>Loading... ⏳</span>;
@@ -23,22 +62,36 @@ function Carlist () {
   }
   else {
     return(
-      <table>
-        <tbody>
-          {
-            data.map((car: CarResponse) => 
-              <tr key={car._links.self.href}>
-                <td>{car.brand}</td>
-                <td>{car.model}</td>
-                <td>{car.color}</td>
-                <td>{car.registrationNumber}</td>
-                <td>{car.modelYear}</td>
-                <td>{car.price}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
+      // <table>
+      //   <tbody>
+      //     {
+      //       data.map((car: CarResponse) => 
+      //         <tr key={car._links.self.href}>
+      //           <td>{car.brand}</td>
+      //           <td>{car.model}</td>
+      //           <td>{car.color}</td>
+      //           <td>{car.registrationNumber}</td>
+      //           <td>{car.modelYear}</td>
+      //           <td>{car.price}</td>
+      //         </tr>
+      //       )
+      //     }
+      //   </tbody>
+      // </table>
+      <>
+        <AddCar />
+        <DataGrid 
+          rows={data}
+          columns={columns}
+          getRowId={row => row._links.self.href}
+          />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message='선택한 자동차 정보가 삭제되었습니다.'
+        />
+      </>
     )
   }
 }
