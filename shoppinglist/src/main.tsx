a5544ef781319ab50544d4c854d15d3b4d0ce2e3
rudals5215 <rudals5215@naver.com>
2025-10-28 10:42:34 +0900

    feat: oauth2 login process

--- shoppinglist/src/main.tsx
+++ shoppinglist/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import GoogleOAuthProvider
+
+const googleClientId = 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
